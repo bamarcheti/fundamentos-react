b271715bd7b5bad026fe2f31825a801724a71d3e
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-03-23 11:13:45 -0400

    corrigindo arquivo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,69 @@
-/* eslint-disable import/no-anonymous-default-export */
-import './App.css'
-import React from "react";
+import React from 'react';
+import './styles/App.css';
 
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import ComFilhos from './components/basicos/ComFilhos'
-import Card from './components/layout/Card'
-import Repeticao from './components/basicos/Repeticao';
+import ComFilhos from './components/basicos/ComFilhos';
+import ComParametro from './components/basicos/ComParametro';
 import Condicional from './components/basicos/Condicional';
 import CondicionalComIf from './components/basicos/CondicionalComIf';
+import Primeiro from './components/basicos/Primeiro';
+import Repeticao from './components/basicos/Repeticao';
 import Pai from './components/comunicacao/direta/Pai';
 import Super from './components/comunicacao/indireta/Super';
-import Input from './components/form/Input';
 import Contador from './components/contador/Contador';
+import Input from './components/form/Input';
+import Card from './components/layout/Card';
 import Mega from './components/mega/Mega';
+import Title from './components/Title';
 
-export default (props) => (
+function App() {
+  return (
     <div className="App">
-        <h1>Funcamentos React</h1>
+      <Title title='Fundamentos React' />
 
-        <div className='Cards'>
+      <div className="Cards">
+        <Card titulo="#01 - Primeiro Componente" color="#92B06A">
+          <Primeiro />
+        </Card>
+        <Card titulo="#02 - Componente Com Parametro" color="#FF85CB">
+          <ComParametro titulo="Esse é o títlo" subtitulo="Esse é o subtítulo" />
+          <ComParametro titulo="Opa" subtitulo="Epa" />
+        </Card>
+        <Card titulo="#03 - Componente Com Filhos" color="#D96459">
+          <ComFilhos>
+            <ul>
+              <li>Ana</li>
+              <li>Bia</li>
+              <li>Carlos</li>
+              <li>Daniel</li>
+            </ul>
+          </ComFilhos>
+        </Card>
+        <Card titulo="#04 - Repetição" color="#008BBA">
+          <Repeticao />
+        </Card>
+        <Card titulo="#05 - Condicional v1" color="#E94C6F">
+          <CondicionalComIf numero={11}></CondicionalComIf>
+        </Card>
+        <Card titulo="#06 - Condicional v2" color="#FA6900">
+          <Condicional numero={10}></Condicional>
+        </Card>
+        <Card titulo="#07 - Comunicação Direta" color="#4298B5">
+          <Pai sobrenome="Freitas"></Pai>
+        </Card>
+        <Card titulo="#08 - Comunicação Indireta" color="#000">
+          <Super />
+        </Card>
+        <Card titulo="#09 - Input" color="#9C0F5F">
+          <Input />
+        </Card>
+        <Card titulo="#10 - Contador" color="#293E6A">
+          <Contador passo={10} valor={100}></Contador>
+        </Card>
         <Card titulo="#11 - Mega" color="#73503C">
-                <Mega qtdNumero={8}/>
-            </Card>
-            <Card titulo="#10 - Contador" color="#293E6A">
-                <Contador passo={10} valor={100}></Contador>
-            </Card>
-            <Card titulo="#09 - Input" color="#9C0F5F">
-                <Input />
-            </Card>
-            <Card titulo="#08 - Comunicação Indireta" color="#000">
-                <Super />
-            </Card>
-            <Card titulo="#07 - Comunicação Direta" color="#4298B5">
-                <Pai sobrenome="Freitas"></Pai>
-            </Card>
-            <Card titulo="#06 - Condicional v2" color="#FA6900">
-                <Condicional numero={10}></Condicional>
-            </Card>
-            <Card titulo="#05 - Condicional v1" color="#E94C6F">
-                <CondicionalComIf numero={11}></CondicionalComIf>
-            </Card>
-            <Card titulo="#04 - Repetição" color="#008BBA">
-                <Repeticao />
-            </Card>
-            <Card titulo="#03 - Componente Com Filhos" color="#D96459">
-                <ComFilhos>
-                    <ul>
-                        <li>Ana</li>
-                        <li>Bia</li>
-                        <li>Carlos</li>
-                        <li>Daniel</li>
-                    </ul>
-                </ComFilhos>
-            </Card>
-            <Card titulo="#02 - Componente Com Parametro" color="#FF85CB">
-                <ComParametro titulo="Esse é o títlo" subtitulo="Esse é o subtítulo" />
-                {/* <ComParametro titulo="Opa" subtitulo="Epa" /> */}
-            </Card>
-            <Card titulo="#01 - Primeiro Componente" color="#92B06A">
-                <Primeiro />
-            </Card>
-        </div>
+          <Mega qtdNumero={8} />
+        </Card>
+      </div>
     </div>
-)
+  )
+}
+export default App
