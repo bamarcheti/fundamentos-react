39301ef2eb336a37f6e81d24eb6d75604eda54e5
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-03-25 15:04:59 -0400

    criando componente ComParametros

--- src/components/ComParametro/index.jsx
+++ src/components/ComParametro/index.jsx
@@ -1,14 +1,15 @@
-/* eslint-disable import/no-anonymous-default-export */
 import React from "react"
 
-export default (props) => {
+function ComParametro(props) {
     // props é somente leitura!
     // props.titulo = "Outro Título";
-    
+
     return (
-        <>
+        <div>
             <h3>{props.titulo}</h3>
             <p>{props.subtitulo}</p>
-        </>
+        </div>
     )
 }
+
+export default ComParametro
