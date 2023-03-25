86062593d2df0dbadd19e8073f422895300b0386
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-03-25 15:03:11 -0400

    criando componente Primeiro

--- src/components/basicos/Primeiro.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import React from "react"
-
-export default () =>
-        <>
-            <h1>Primeiro Componente (Arrow)</h1>
-            <h2>Exemplo de um componente React</h2>
-        </>
-
